830cee122cfc1007193cde32a09ee156885ead38
jnaveen27 <141658603+jnaveen27@users.noreply.github.com>
2025-04-11 10:48:24 +0530

    menu first click bug fix

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 function showMenu(){
     let menu = document.getElementById('folio-navbox-parent');
-    console.log(menu.style.display)
 
-    if(menu.style.display === "none"){
+    if(menu.style.display === "none" || menu.style.display === ""){
         menu.style.display = "block";
     }else {
         menu.style.display = "none";
